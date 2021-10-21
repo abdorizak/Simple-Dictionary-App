6caa0968b73ea365a3998cfb0d06b8e783e40331
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-10-21 16:45:06 +0300

    Simple Dictionary App

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Simple-Dictionary-App
+
+###this is Simple Dictionary App   
+``Dictionary<String:String>``
+```
+    [ "Boy":"Wiil", "Girl":"Gabar", "Tree":"Geed", "Street":"Wado", "Car":"Baabur", "Gift":"Hadiyad", "Window":"Daaqad","Dog":"Ey"
+    ]
+```
+the first key is string and value is also string
+where logic comes when user finds the key if that found will display value that key for example ``Boy`` is key will find in dictionary if isfound then will display value that key is ``Wiil``
+
--- Simple Dictionary App/ViewController.swift
+++ Simple Dictionary App/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var inputField: UITextField!
+    
+    @IBOutlet var findBtn: UIButton!
+    
+    @IBOutlet var answerLabel: UILabel!
+    
+    
+    var englishToSomali:[String:String] = ["Boy":"Wiil",
+                                           "Girl":"Gabar",
+                                           "Tree":"Geed",
+                                           "Street":"Wado",
+                                           "Car":"Baabur",
+                                           "Gift":"Hadiyad",
+                                           "Window":"Daaqad",
+                                           "Dog":"Ey"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        findBtn.layer.cornerRadius = 10
     }
 
-
+    
+    func translate() {
+        
+        guard inputField.text?.isEmpty == false else { return print("Cannot be Empty")}
+        
+        if (englishToSomali[inputField.text!] != nil) {
+            answerLabel.text = englishToSomali[inputField.text!] ?? "nil"
+        } else {
+            answerLabel.text = "Not Found"
+        }
+    }
+    
+    @IBAction func didTapFind(_ sender: UIButton) {
+        translate()
+    }
+    
+    
+    
+    
 }
 
